ec0dfc3fdc7af72ab5a5d0a526fcf31e4f83bfb0
Legendary Emoji <60050010+LegendaryEmoji@users.noreply.github.com>
2020-12-11 02:28:06 +0500

    Update :D

--- index.js
+++ index.js
@@ -1,29 +1,36 @@
-const { Token, ChannelID } = require("./config.js");
+const { Token, ChannelID, SnowflakeApiToken } = require("./config.js");
 const Discord = require("discord.js");
-const Got = require("got");
+const Fetch = require("node-fetch");
 const Client = new Discord.Client();
 
 //Ready Event
 Client.on("ready", async () => {
     console.clear();
     console.log(`Simple Chat Bot - ${Client.user.username} - Ready\nBy Legendary Emoji`);
-    await Client.user.setActivity(`Prefix - ${Prefix}`, {
+    await Client.user.setActivity(`Chatting With Members :D`, {
         type: "PLAYING"
     });
 });
 
 //Message Event
 Client.on("message", async message => {
-    if (message.author.bot || message.webhookID || message.channel.type === "dm") return;
+    if (message.author.bot || !message.guild || message.webhookID || !SnowflakeApiToken) return;
     if (ChannelID && ChannelID.toLowerCase() !== "put channel id - only put if you want bot work in 1 channel!") {
         if (ChannelID !== message.channel.id) return;
     };
-    try {
-        const res = await Got(`https://api.snowflakedev.xyz/chatbot?message=${Discord.Util.escapeMarkdown(message.content)}`); // https://discord.gg/uqB8kxh
-        const json = await JSON.parse(res.body);
-        return message.channel.send(json.message);
-    } catch (error) {
-        return message.channel.send(`Something Went Wrong, Try Again Later!`).then(() => console.log(error));
+
+    if (message.content) {
+        try {
+            const res = await Fetch(`http://api.snowflakedev.cf:9019/api/chatbot?message=${encodeURIComponent(message.content)}`, {
+                headers: {
+                    "Authorization": String(SnowflakeApiToken)
+                }
+            }); // https://discord.gg/uqB8kxh
+            const json = await res.json(); 
+            return message.channel.send(json.message);
+        } catch (error) {
+            return message.channel.send("Something Went Wrong, Try Again Later!").then(() => console.log(error));
+        };
     };
 });
 
