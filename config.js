7005e252852ad64789a6451e3a97c6252000c7ab
Legendary Emoji <60050010+LegendaryEmoji@users.noreply.github.com>
2020-12-11 02:33:09 +0500

    Update :D

--- config.js
+++ config.js
@@ -1,2 +1,14 @@
-exports.Token = `Put Your Bot Token Here!`;
-exports.ChannelID = `Put Channel ID - Only Put If You Want Bot Work In 1 Channel!`;
+module.exports = {
+   Token: "Put Your Discord Bot Token Here - Important",
+   SnowflakeApiToken: "Put Your Api Key - Important",
+   ChannelID: "Put Channel ID - Only Put If You Want Bot Work In 1 Channel!",
+};
+
+/*
+Q. How To Get Token? [Required]
+A. Please Check This - https://discordjs.guide/preparations/setting-up-a-bot-application.html#your-token
+Q. How To Get Snowflake Api Token? [Required, Bot Will Not Work If You Don't Provide Your Api Key]
+A. Please Join This Discord Server - https://discord.gg/uqB8kxh | After Joining, Ask For Api Key
+Q. How To Get Channel ID? [Optional]
+A. In The Discord Application Go To Settings > Appearance > Turn On Developer Mode, To Get ID: Right Click Channel Name & Copy The Channel ID (Copy ID)
+*/
