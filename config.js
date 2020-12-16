bc6c0fc083e49a9510555812f2a8746c394f9a58
Legendary Emoji <60050010+LegendaryEmoji@users.noreply.github.com>
2020-12-16 11:49:40 +0500

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,7 @@
 Q. How To Get Token? [Required]
 A. Please Check This - https://discordjs.guide/preparations/setting-up-a-bot-application.html#your-token
 Q. How To Get Snowflake Api Token? [Required, Bot Will Not Work If You Don't Provide Your Api Key]
-A. Please Join This Discord Server - https://discord.gg/uqB8kxh | After Joining, Ask For Api Key
+A. Please Join This Discord Server - https://discord.gg/uqB8kxh | After Joining, Ask For Api Key | If Website Is Down Then You Can't Do Anything
 Q. How To Get Channel ID? [Optional]
 A. In The Discord Application Go To Settings > Appearance > Turn On Developer Mode, To Get ID: Right Click Channel Name & Copy The Channel ID (Copy ID)
 */
