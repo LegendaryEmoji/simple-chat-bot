c8e7adf0b72790b52e13ae04b2b9f3f2442d9b91
Legendary Emoji <60050010+LegendaryEmoji@users.noreply.github.com>
2021-01-01 01:20:17 +0500

    Another Update :D

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const Client = new Discord.Client();
 //Ready Event
 Client.on("ready", async () => {
     console.clear();
-    console.log(`Simple Chat Bot - ${Client.user.username} - Ready\nBy Legendary Emoji`);
+    console.log(`Simple Chat Bot - ${Client.user.username} - Ready`);
     await Client.user.setActivity(`Chatting With Members :D`, {
         type: "PLAYING"
     });
